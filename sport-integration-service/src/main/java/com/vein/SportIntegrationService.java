29422ba93464e84eb635191cc302424143fc7d3d
steyoz <samet.zyurek@gmail.com>
2025-01-06 02:35:08 +0300

    base sport integration service microservice creation completed

---
+++ sport-integration-service/src/main/java/com/vein/SportIntegrationService.java
@@ -0,0 +1,11 @@
+package com.vein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportIntegrationService {
+    public static void main(String[] args) {
+        SpringApplication.run(SportIntegrationService.class, args);
+    }
+}
